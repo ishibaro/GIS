e61ce8de53b2d4122b57bcf055defe0f8175e204
Ishiba RO <israel.hinojosa-balino@durham.ac.uk>
2024-03-22 12:17:40 +0000

    Add files via upload
    new files and reclass files for grass 8

---
+++ WindDirection/environs.bat
@@ -0,0 +1,36 @@
+REM C:\Program Files\AppJ\QGIS 3.16\bin
+REM
+REM Environmental variables for GRASS OSGeo4W installer
+REM
+
+set GISBASE=C:\PROGRA~1\AppJ\QGIS32~1.14\apps\grass\grass83
+set GRASS_PROJSHARE=C:\PROGRA~1\AppJ\QGIS32~1.14\share\proj
+set OSGEO4W_ROOT=C:\PROGRA~1\AppJ\QGIS32~1.14
+set PATH=C:\PROGRA~1\AppJ\QGIS32~1.14\apps\grass\grass83\gui\scripts;C:\PROGRA~1\AppJ\QGIS32~1.14\apps\grass\grass83\lib;C:\PROGRA~1\AppJ\QGIS32~1.14\apps\grass\grass83\bin;C:\Users\cxlz59\AppData\Roaming\GRASS8\addons\bin;C:\PROGRA~1\AppJ\QGIS32~1.14\apps\qt5\bin;C:\PROGRA~1\AppJ\QGIS32~1.14\apps\Python39\Scripts;C:\PROGRA~1\AppJ\QGIS32~1.14\bin;C:\WINDOWS\system32;C:\WINDOWS;C:\WINDOWS\system32\WBem
+
+REM set Path=C:\ProgramData\Anaconda3;C:\ProgramData\Anaconda3\Library\mingw-w64\bin;C:\ProgramData\Anaconda3\Library\usr\bin;C:\ProgramData\Anaconda3\Library\bin;C:\ProgramData\Anaconda3\Scripts;C:\ProgramData\Anaconda3\bin;C:\ProgramData\Anaconda3\condabin;C:\WINDOWS\system32;C:\WINDOWS;C:\WINDOWS\System32\Wbem;C:\WINDOWS\System32\WindowsPowerShell\v1.0;C:\WINDOWS\System32\OpenSSH;C:\Program Files\dotnet;C:\Users\cxlz59\AppData\Local\Microsoft\WindowsApps;C:\Program Files\AppJ\Microsoft VS Code\bin;C:\Users\cxlz59\AppData\Roaming\TinyTeX\bin\windows;C:\Program Files\AppJ\QGIS 3.28.14\bin
+
+set GDAL_DATA=C:\PROGRA~1\AppJ\QGIS32~1.14\apps\gdal\share\gdal
+set GDAL_DRIVER_PATH=C:\PROGRA~1\AppJ\QGIS32~1.14\apps\gdal\lib\gdalplugins
+set GISBASE=C:\PROGRA~1\AppJ\QGIS32~1.14\apps\grass\grass83
+
+REM GISRC=C:\Users\cxlz59\AppData\Local\Temp\grass8-cxlz59-27068\gisrc
+
+set GRASS_ADDON_BASE=C:\Users\cxlz59\AppData\Roaming\GRASS8\addons
+@REM set GRASS_ENCODING=UTF-8
+@REM set GRASS_GNUPLOT=gnuplot -persist
+@REM set GRASS_HTML_BROWSER=start
+@REM set GRASS_MESSAGE_FORMAT=gui
+@REM set GRASS_PAGER=more
+set GRASS_PROJSHARE=C:\PROGRA~1\AppJ\QGIS32~1.14\share\proj
+set GRASS_PYTHON=C:\PROGRA~1\AppJ\QGIS32~1.14\bin\python3.exe
+@REM set GRASS_VERSION=8.3.1
+set GS_LIB=C:\PROGRA~1\AppJ\QGIS32~1.14\apps\gs\lib
+
+REM PDAL_DRIVER_PATH=C:\PROGRA~1\AppJ\QGIS32~1.14\apps\pdal\plugins
+REM POSTGIS_ENABLE_OUTDB_RASTERS=1
+REM POSTGIS_GDAL_ENABLED_DRIVERS=GTiff PNG JPEG GIF XYZ DTED USGSDEM AAIGrid
+
+set PROJ_LIB=C:\PROGRA~1\AppJ\QGIS32~1.14\share\proj
+set PYTHONHOME=C:\PROGRA~1\AppJ\QGIS32~1.14\apps\Python39
+set PYTHONPATH=C:\PROGRA~1\AppJ\QGIS32~1.14\apps\grass\grass83\etc\python;C:\PROGRA~1\AppJ\QGIS32~1.14\apps\grass\grass83\etc\python;
